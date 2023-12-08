b6a5b1514aea469efe30d398f59223671088b84f
MaximKozyro <132302389+MaximKozyro@users.noreply.github.com>
2023-12-08 22:32:08 +0300

    Initial commit

---
+++ Practic.js
@@ -0,0 +1,40 @@
+//Задачка 1
+let number = 1;
+
+if(number < 0){
+    console.log("Число отрицательное");
+} else {
+    console.log("Число положительное");
+}
+
+//Задачка 2
+let number1 = 7;
+
+if(number1 % 2 === 0){
+    console.log("Число четное");
+} else {
+    console.log("Число не четное");
+}
+
+//Задачка 3
+let description1 = "zmych";
+let description2 = "zmychara";
+
+if(description1[0] === description2[1]){
+    console.log("первые символы совпадают");
+} else {
+    console.log("Первые символы не совпадают")
+}
+
+//Задачка 4
+for(let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
+
+//js длядетей массив
+
+let myGuns = ["akm","vss","svd","saiga"];
+myGuns[0] = "vsk94"
+console.log(myGuns[0]);
+
